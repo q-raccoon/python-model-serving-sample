e7d2d14fc944719f8831e00c0ea61536518d20a1
lyzqm123 <lyzqm@naver.com>
2022-06-02 16:53:28 +0900

    Pass the buck of the handling prediction to `handler` class.
    Signed-off-by: lyzqm123 <lyzqm@naver.com>

--- app/predict.py
+++ app/predict.py
@@ -1,8 +1,6 @@
-from glob import glob
 from process.preprocess import normalize
 from loguru import logger
 from model.mnist.mnist_handler import MnistHandler
-import numpy as np
 
 mnist_handler = None
 def get_predictions(image):
@@ -14,8 +12,7 @@ def get_predictions(image):
     logger.debug("Image: [{}]".format(image.shape))
     normalized_image = normalize(image)
 
-    logits = mnist_handler.handler(normalized_image)["dense_1"]
-    max_logits = int(np.argmax(logits[0]))
-    confidence = float(logits[0][max_logits])
-    logger.info("predict: {}, confidence: {}".format(max_logits, confidence))
-    return max_logits, confidence
+    predictions = mnist_handler.handler(normalized_image)
+
+    logger.info("{}".format(predictions))
+    return predictions
