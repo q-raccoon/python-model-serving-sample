e05c706fd5cb6be9275e51a5044ec1e9d8902e70
lyzqm123 <lyzqm@naver.com>
2022-06-12 13:37:11 +0900

    Applied `coco2017_label_parser` into `efficientdet_lite_v4_inference`. - Modified NMS score threshold "0.5" to "0.4".
    Signed-off-by: lyzqm123 <lyzqm@naver.com>

--- app/model/coco2017/efficientdet_lite_v4_inference.py
+++ app/model/coco2017/efficientdet_lite_v4_inference.py
@@ -5,19 +5,24 @@ from ..inference import Inference
 from PIL import Image
 
 class EfficientDetLiteV4Inference(Inference):
-    def __init__(self, draw_image: bool) -> None:
+    def __init__(self, label_parser, draw_image: bool) -> None:
         self.handler_ = EfficientDetLiteV4Handler()
+        self.coco2017_label_parser_ = label_parser
         self.draw_image_ = draw_image
         super().__init__()
 
     def call(self, image: Image.Image):
         preprocessed_image = self.preprocess(image)
         predictions =  self.handler_(preprocessed_image)
-        bboxes, _, _ = self.postprocess(predictions)
+        bboxes, _, classes = self.postprocess(predictions)
 
         if self.draw_image_:
             self.draw_bounding_boxes(images=preprocessed_image, bboxes=bboxes)
-        return bboxes
+
+        np_str_classes = self.coco2017_label_parser_.get_label(classes.numpy())
+        np_str_classes = np.expand_dims(np_str_classes, axis=-1)
+        bboxes_and_labels = np.concatenate([bboxes.numpy(), np_str_classes], axis=-1)
+        return bboxes_and_labels.tolist()
 
     def preprocess(self, image: Image.Image) -> tf.image:
         resized_image = np.expand_dims(np.array(image.resize((640, 640))), axis=0)
@@ -29,7 +34,7 @@ class EfficientDetLiteV4Inference(Inference):
         scores = predictions["output_1"][0]
         classes = predictions["output_2"][0]
         
-        selected_boxes_index = tf.image.non_max_suppression(boxes, scores, max_output_size=10, iou_threshold=0.5, score_threshold=0.5)
+        selected_boxes_index = tf.image.non_max_suppression(boxes, scores, max_output_size=10, iou_threshold=0.5, score_threshold=0.4)
         selected_boxes = tf.gather(boxes, selected_boxes_index)
         selected_scores = tf.gather(scores, selected_boxes_index)
         selected_classes = tf.gather(classes, selected_boxes_index)
@@ -37,6 +42,7 @@ class EfficientDetLiteV4Inference(Inference):
         selected_boxes = tf.expand_dims(selected_boxes, axis=0)
         selected_scores = tf.expand_dims(selected_scores, axis=0)
         selected_classes = tf.expand_dims(selected_classes, axis=0)
+        
         return selected_boxes, selected_scores, selected_classes
     
     def draw_bounding_boxes(self, images: tf.image, bboxes: tf.Tensor):
